05927bb140eb3bf6670d095f749566b7ee7dbf17
Éric NICOLAS <ccjmne@gmail.com>
2024-12-01 14:36:56 +0100

    Implement day 1 part 1

--- src/y2024/d1.rs
+++ src/y2024/d1.rs
@@ -1,6 +1,24 @@
-pub fn part1(input: String) {
-    println!("Running Part 1!");
-    println!("{}", input);
+use regex::Regex;
+
+pub fn part1(input: String) -> u32 {
+    let parser = Regex::new(r"^(?P<a>\d+)\s*(?P<b>\d+)$").expect("Invalid regex");
+    let (mut l, mut r): (Vec<u32>, Vec<u32>) = input
+        .lines()
+        .filter_map(|line| {
+            parser.captures(line).and_then(|caps| {
+                let a = caps.name("a")?.as_str().parse::<u32>().ok()?;
+                let b = caps.name("b")?.as_str().parse::<u32>().ok()?;
+                Some((a, b))
+            })
+        })
+        .unzip();
+    l.sort();
+    r.sort();
+
+    l.into_iter()
+        .zip(r)
+        .map(|(a, b)| (a).abs_diff(b))
+        .sum::<u32>()
 }
 
 pub fn part2(input: String) {
