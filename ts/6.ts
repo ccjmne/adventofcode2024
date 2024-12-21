93dd2fd80db7ebecf42b2ef6e04bf736a5875728
Éric NICOLAS <ccjmne@gmail.com>
2024-12-21 14:01:57 +0100

    Beautify day 6 somewhat

--- ts/6.ts
+++ ts/6.ts
@@ -7,12 +7,12 @@ type Dir = 'L' | 'D' | 'U' | 'R' | '?' | '!' // '?' means stuck in a loop, '!' m
 type Cell = [blocked: boolean, been: string] // `been` contains at most one of each of L, D, U, R
 type Guard = [row: number, col: number, dir: Dir]
 
-const map: Cell[][] = data.replace('^', '.').split(/\n/g).map(line => [...line].map(c => [c === '#', '']))
-const guard: Guard = [0 | data.indexOf('^') / (map[0].length + 1), data.indexOf('^') % (map[0].length + 1), 'U']
+const map: Cell[][] = data.split(/\n/g).map(line => [...line].map(c => [c === '#', '']))
+const start: Guard = [0 | data.indexOf('^') / (map[0].length + 1), data.indexOf('^') % (map[0].length + 1), 'U']
 
 loop(map)
-console.log(map.reduce((sum, row   ) => sum + row.filter(([, been]   ) => !!been                          ).length, 0))
-console.log(map.reduce((sum, row, r) => sum + row.filter(([, been], c) => !!been && loop(block(map, r, c))).length, 0))
+console.log(map.flatMap((row   ) => row.filter(([, been]   ) => !!been))                          .length)
+console.log(map.flatMap((row, r) => row.filter(([, been], c) => !!been && loop(block(map, r, c)))).length)
 
 function step(map: Cell[][], [row, col, dir]: Guard): Guard {
   if (map[row][col][1].includes(dir)) return [row, col, '?']; else map[row][col][1] += dir
@@ -25,11 +25,11 @@ function step(map: Cell[][], [row, col, dir]: Guard): Guard {
   }
 }
 
-function loop(map: Cell[][], g: Guard = guard): boolean {
-  while (!/[!?]/.test(g[2])) g = step(map, g)
-  return g[2] === '?'
+function loop(map: Cell[][], guard: Guard = start): boolean {
+  while (!/[!?]/.test(guard[2])) guard = step(map, guard)
+  return guard[2] === '?'
 }
 
-function block(map: Cell[][], brow: number, bcol: number): Cell[][] {
-  return map.map((row, r) => row.map(([blocked], c) => [blocked || (brow === r && bcol === c), ''] as Cell).slice())
+function block(map: Cell[][], br: number, bc: number): Cell[][] {
+  return map.map((row, r) => row.map(([blocked], c) => [blocked || (br === r && bc === c), ''] as Cell))
 }
