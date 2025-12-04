8a5384be0949769d9dfc8df64dc26f95dec90278
Éric NICOLAS <ccjmne@gmail.com>
2025-12-04 21:47:57 +0100

    Polish 2025 TS solutions

--- ts/2025-1.ts
+++ ts/2025-1.ts
@@ -1,15 +1,15 @@
 // https://adventofcode.com/2025/day/1
-import { readFile } from 'fs/promises'
+import { readFile } from 'node:fs/promises'
 const __dirname = import.meta.url.substring('file://'.length, import.meta.url.lastIndexOf('/'))
 const data = String(await readFile(__dirname + '/2025-1.txt')).trim()
-const movs = data.split(/\n/).map(move => move.substring(1) * (move[0] === 'L' ? -1 : 1))
+  .split(/\n/).map(move => move.substring(1) * (move.startsWith('L') ? -1 : 1))
 
-console.log(movs.reduce(
+console.log(data.reduce(
   ({ dial, ctr }, d) => ({ dial: dial + d, ctr: ctr + !mod100(dial + d) }),
   { dial: 50, ctr: 0 }
 ).ctr)
 
-console.log(movs.reduce(({ dial, ctr }, d) => {
+console.log(data.reduce(({ dial, ctr }, d) => {
   let over = Math.floor(Math.abs(d / 100))
   let next = mod100(dial + d)
   return { dial: next, ctr: ctr + over + (next === 0 || dial !== 0 && Math.sign(d) !== Math.sign(next - dial)) }
--- ts/2025-2.ts
+++ ts/2025-2.ts
@@ -4,23 +4,23 @@ const __dirname = import.meta.url.substring('file://'.length, import.meta.url.la
 console.log(String(await readFile(__dirname + '/2025-2.txt')).trim()
   .split(/,/).map(range => range.split(/-/))
   .flatMap(([lo, hi]) => range(lo.length, hi.length).flatMap((len, i, { length }) => {
-    const lol = ((i === 0          && lo.length === len) ? lo.substring(lo.length - len) : 1 + '0'.repeat(len - 1))
-    const hil = ((i === length - 1 && hi.length === len) ? hi.substring(hi.length - len) : '9'.repeat(len))
+    const lol = (i === 0          && lo.length === len) ? lo.substring(lo.length - len) : 1 + '0'.repeat(len - 1)
+    const hil = (i === length - 1 && hi.length === len) ? hi.substring(hi.length - len) : '9'.repeat(len)
     return by(2) // comment out for step 2
     return uniq(range(2, len).flatMap(by))
-    function by(n: number): number[] {
+    function by(n: number) {
       if (lol.length % n !== 0) return []
       return range(lol.substring(0, len / n), hil.substring(0, len / n))
-        .map(m => Number(String(m).repeat(n)))
+        .map(pat => Number(String(pat).repeat(n)))
         .filter(x => x >= lo && x <= hi)
     }
   }))
   .reduce((sum, n) => sum + n, 0))
 
-function range(lo: any, hi: any): number[] {
-  return Array.from({ length: hi - lo + 1}).map((_, i) => Number(lo) + i)
+function range(lo: any, hi: any) {
+  return Array.from({ length: hi - lo + 1 }).map((_, i) => Number(lo) + i)
 }
 
-function uniq(arr: number[]): number[] {
+function uniq(arr: number[]) {
   return Object.keys(arr.reduce((mem, v) => Object.assign(mem, { [v]: true }), {})).map(Number)
 }
